f2032a254e8d185e2b8a6aa739f66f18d9e46174
sebasolr <seba.olr@gmail.com>
2022-05-30 12:13:18 -0400

    saludo bienvenida

--- asincro.js
+++ asincro.js
@@ -1,4 +1,5 @@
-let edad = prompt('ingresa tu edad?');
+let nombre = prompt('Hola, nos podrias escribir tu nombre');
+let edad = prompt('bienvenido ' + nombre + ' ingresa tu edad?');
 alert('wooah tienes ' + edad *365 + ' dias')
 
 let edad_maxima = prompt('cuantos anos crees que vas a vivir?');
@@ -8,3 +9,4 @@ let restante = edad_maxima - edad;
 let total_snack = restante * 365;
 let snack_semanal = parseInt(total_snack / 7);
 alert(`te quedan exactamente ${snack_semanal} ${snack} por el resto de tu vida`);
+// holi :)
